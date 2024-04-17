ada5792c29ffb7a4c9d60dc11f88160172faac24
pavithraa20102004 <pavithraa20102004@gmail.com>
2024-04-17 09:01:21 +0530

    array length added

--- arraycc.java
+++ arraycc.java
@@ -2,17 +2,20 @@ import java.util.*;
 public class arraycc{
 public static void main(String[] args){
     int marks[] = new int[4];
+
+    //length of an array
+    System.out.println("Length of the array : "+marks.length);
     
     // Storing elements in the array
-    Scanner sc = new Scanner(System.in);
-    marks[0]=sc.nextInt();
-    marks[1]=sc.nextInt();
-    marks[2]=sc.nextInt();
+    // Scanner sc = new Scanner(System.in);
+    // marks[0]=sc.nextInt();
+    // marks[1]=sc.nextInt();
+    // marks[2]=sc.nextInt();
 
     //output
-    System.out.println("Tamil: "+ marks[0]);
-    System.out.println("English: "+ marks[1]);
-    System.out.println("Math: "+ marks[2]);
+    // System.out.println("Tamil: "+ marks[0]);
+    // System.out.println("English: "+ marks[1]);
+    // System.out.println("Math: "+ marks[2]);
 
     // marks[2] =100; Declaring a new value for Math
     //This will not affect the older mark of Math.
@@ -24,7 +27,7 @@ public static void main(String[] args){
     // System.out.println("Updated value = " + marks[2]);
 
     //To calculate percentage
-    int percentage = (marks[0]+marks[1]+marks[2])/3;
-    System.out.println("Percentage  = "+percentage+"%");
+    // int percentage = (marks[0]+marks[1]+marks[2])/3;
+    // System.out.println("Percentage  = "+percentage+"%");
     }
 }
