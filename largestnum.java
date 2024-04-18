7084fd36a503eba9503a293ea4034b917b693989
pavithraa20102004 <pavithraa20102004@gmail.com>
2024-04-18 09:51:39 +0530

    added smallest number

--- largestnum.java
+++ largestnum.java
@@ -1,15 +1,21 @@
 public class largestnum {
     public static int getLargest(int numbers[]){
-        int largest = Integer.MIN_VALUE; // Initializing the variable with minimum possible integer value
+        int largest = Integer.MIN_VALUE; // -infinity
+        int smallest = Integer.MAX_VALUE; //+infinity
         for(int i=0;i<numbers.length;i++){
             if(largest < numbers[i]){
                 largest = numbers[i]; // Updating the largest number if current number is greater than largest
             }
+            if(smallest > numbers[i]){
+                smallest = numbers[i]; //Updating
+            }
+            
         }
+        System.out.println("Smallest number: " + smallest); //printing smallest number
         return largest;
     }
     public static void main(String[] args) {
-        int numbers[] = {1,2,6,13,5};
+        int numbers[] = {1,-9,6,13,5};
         System.out.println("The Largest number is: " + getLargest(numbers));
     }
     
