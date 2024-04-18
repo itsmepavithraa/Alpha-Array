a822f38e04471563044567f0782302cfbc63da57
pavithraa20102004 <pavithraa20102004@gmail.com>
2024-04-18 19:39:43 +0530

    add total pairs

--- pairs.java
+++ pairs.java
@@ -1,12 +1,15 @@
 public class pairs {
     public static void printPairs(int numbers[]){
+        int tp = 0;
         for(int i=0;i<numbers.length;i++){ //outer loop
             int curr = numbers[i]; //2 4 6 8 10
             for(int j=i+1;j<numbers.length;j++){ //inner loop // j=i+1(incase of 2 we get j as 4,6,8,10)
                 System.out.print("(" + curr + "," + numbers[j]+ ")"); //printing
+                tp++;
             }
             System.out.println(); //next line
         }
+        System.out.println("Total pairs: "+ tp);
     }
     public static void main(String[] args) {
         int numbers[] = {2,4,6,8,10};
